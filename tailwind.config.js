61ab0df476cfc307a5c92cbbd9b587a47bf8c5a8
daniela-ghenu <daniela.andreea.ghenu@gmail.com>
2024-08-15 09:50:22 +0300

    Customize tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '375px',
+      'md': '768px',
+      'lg': '1440px'
+    },
+    colors: {
+      "purple": "#A729F5",
+      "dark-navy": "#313E51",
+      "navy": "#3B4D66",
+      "grey-navy": "#626C7F",
+      "light-bluish": "#ABC1E1",
+      "light-grey": "#F4F6FA",
+      "white": "#FFFFFF",
+      "green": "#26D782",
+      "red": "#EE5454"
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        sm: '0',
+      },
+      screens: {
+        sm: '640px',
+        lg: '1160px',
+      }
+    },
+    fontFamily: {
+      sans: ["Rubik", 'sans-serif'],
+      serif: ["Rubik", 'serif'],
+    },
   },
+
   plugins: [],
 }
 
