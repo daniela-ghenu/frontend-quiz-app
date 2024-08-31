cecc1ae1bc97f617c182b52ac49acd144f55b1e8
daniela-ghenu <daniela.andreea.ghenu@gmail.com>
2024-08-31 21:05:05 +0300

    Finish initial quiz page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,24 +10,42 @@ export default {
       '2xl': '1440px'
     },
     colors: {
-      "purple": "#A729F5",
-      "dark-navy": "#313E51",
-      "navy": "#3B4D66",
-      "grey-navy": "#626C7F",
-      "light-bluish": "#ABC1E1",
-      "light-grey": "#F4F6FA",
-      "white": "#FFFFFF",
-      "green": "#26D782",
-      "red": "#EE5454"
+      'purple': '#A729F5',
+      'dark-navy': '#313E51',
+      'navy': '#3B4D66',
+      'grey-navy': '#626C7F', 
+      'light-bluish': '#ABC1E1',
+      'light-grey': '#F4F6FA',
+      'white': '#FFFFFF',
+      'green': '#26D782',
+      'red': '#EE5454'   
     },
     container: {
       center: true,
-      padding: '1.5rem',
+      padding: {
+        DEFAULT: '1.5rem',
+        sm: '0',
+      },
     },
     fontFamily: {
       sans: ["Rubik", 'sans-serif'],
       serif: ["Rubik", 'serif'],
     },
+
+    extend: {
+      boxShadow: {
+        'card': '0 16px 40px 0 rgba(143, 160, 193, 0.14)',
+        'card-hover': '0 16px 50px 0 rgba(143, 160, 193, 0.3);',
+      },
+      backgroundImage: {
+        'mobile-dark': "url('/images/pattern-background-mobile-dark.svg')",
+        'mobile-light': "url('/images/pattern-background-mobile-light.svg')",
+        'tablet-dark': "url('/images/pattern-background-tablet-dark.svg')",
+        'tablet-light': "url('/images/pattern-background-tablet-light.svg')",
+        'desktop-dark': "url('/images/pattern-background-desktop-dark.svg')",
+        'desktop-light': "url('/images/pattern-background-desktop-light.svg')",
+      }
+    }
   },
 
   plugins: [
